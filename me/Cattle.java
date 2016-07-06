7dd82e6a3b57d17f5b5c129078e439c608a616fa
luoyafei <luoyafei.newinstance@foxmail.com>
2016-07-06 23:47:39 +0800

    add something

--- me/Cattle.java
+++ me/Cattle.java
@@ -1,5 +1,21 @@
-public class Cattle {
+public class Cattle implements ICattle {
 	
+	public boolean isCow;
+	public int age;
+	public int procreateCount;
+	
+	@Override
+	public boolean canProcreate(boolean isCow, int age) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int addProcreateCount(boolean canProcreate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
